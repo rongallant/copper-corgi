795c3eeb3108d1b534d053fe797225a80a4e5ef2
Ron Gallant <ron@rongallant.com>
2018-06-06 21:36:02 -0400

    Create login page.

---
+++ src/screens/login.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class LoginPage extends Component {
+
+  render() {
+    return (
+        <View>
+          <Text>Welcome to the login page.</Text>
+        </View>
+    );
+  }
+}
