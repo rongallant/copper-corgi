4531f1d3e7e38d4de4f3d5ef9806d2e3039c0c3d
Ron Gallant <ron@rongallant.com>
2018-09-08 23:20:25 -0400

    Fix merge issues

---
+++ src/services/authentication.js
@@ -0,0 +1,10 @@
+
+
+export const login = (username, password) => {
+}, isLoggedIn = () => {
+	const userAuth = localStorage.getItem("userAuth");
+	return !!userAuth;
+}, logout = () => {
+	localStorage.removeItem("userAuth");
+	return false;
+};
